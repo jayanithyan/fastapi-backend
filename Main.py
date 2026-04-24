1d4f08bd5dc4bc1ebb24a14b4fbc33aada26406b
jayanithyan <srisarankamalyarns83@gmail.com>
2026-04-24 11:52:50 +0530

    new method created

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
-#def root():
+def root():
     return {"message":"Hello World"}
+@app.get("/post")
+def get_post():
+    return {"message":"This is the list of posts"}
