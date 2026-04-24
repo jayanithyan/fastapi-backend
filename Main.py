1453d8e2fbd012c04277fa27baf2bad6f46b06d6
jayanithyan <srisarankamalyarns83@gmail.com>
2026-04-24 12:00:37 +0530

    post method created

--- Main.py
+++ Main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi import Body
+
+
+
 app=FastAPI()
 @app.get("/")
 def root():
     return {"message":"Hello World"}
+
+
+
 @app.get("/post")
 def get_post():
     return {"message":"This is the list of posts"}
+
+
+
+@app.post("/createposts")
+def create_post(payLoad: dict =Body(...)):
+    return {"message":"Post created successfully"}
