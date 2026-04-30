1002d70b810cc5c5a68af10c5f683bf520189b15
jayanithyan <srisarankamalyarns83@gmail.com>
2026-04-30 16:22:18 +0530

    new method created

--- Main.py
+++ Main.py
@@ -28,6 +28,13 @@ def find_post(id):
 
 
 
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p["id"]==id:
+            return i
+
+
+
 @app.get("/")
 def root():
     return {"message":"Hello World"}
@@ -40,7 +47,7 @@ def get_posts():
 
 
 
-@app.post("/posts")
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
     post_dict=post.dict()
     post_dict["id"]=randrange(0,100000000)
@@ -55,3 +62,12 @@ def get_post(id:int):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return{"post_detail":f"post with id: {id}"}
+
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    index=find_index_post(id)
+    if index==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
