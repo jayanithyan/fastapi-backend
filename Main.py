f6c724421ebb331668795bd38b1da2864c2b5230
jayanithyan <srisarankamalyarns83@gmail.com>
2026-04-30 15:51:40 +0530

    new method initilaized

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
+
 
 
 app=FastAPI()
@@ -16,6 +18,11 @@ class Post(BaseModel):
 my_posts=[{"title":"title of post 1","content":"content of post 1","id":1},{"title":"fav food","content":"briyani","id":3}]
 
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"]==id:
+            return p
+
 @app.get("/")
 def root():
     return {"message":"Hello World"}
@@ -24,12 +31,21 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"message":"This is the list of posts"}
+    return {"message":my_posts}
 
 
 
 @app.post("/posts")
 def create_posts(post:Post):
-    print(post)
-    print(post.dict())
-    return{"data":post}
+    post_dict=post.dict()
+    post_dict["id"]=randrange(0,100000000)
+    my_posts.append(post_dict)
+    return{"data":post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post=find_post(int(id))
+    if not post:
+        return{"error":"post not found"}
+    return{"post_detail":f"post with id: {id}"}
