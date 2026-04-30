c09bdb8afb102bbf1f735ad0ff1e272e1421e981
jayanithyan <srisarankamalyarns83@gmail.com>
2026-04-30 15:24:08 +0530

    new variable intialised

--- Main.py
+++ Main.py
@@ -1,9 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-
+from pydantic import BaseModel
 
 
 app=FastAPI()
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published:bool=True
+    rating: Optional[int]=None
+
 @app.get("/")
 def root():
     return {"message":"Hello World"}
@@ -17,6 +25,6 @@ def get_posts():
 
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"message":"Post created successfully"}
+def create_posts(new_post:Post):
+    print(new_post)
+    return{"data":"new post created"}
